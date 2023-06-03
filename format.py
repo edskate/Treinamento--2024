167b9aaed02189a6652a6b5abbdfb472ba5d4449
edilson gomes de lima <edskateicevida@gmail.com>
2023-06-03 11:26:46 -0300

    add calculo de distancia

---
+++ format.py
@@ -0,0 +1,10 @@
+def calcular_tempo_viagem(distancia, velocidade_media):
+    tempo = distancia / velocidade_media
+    return tempo
+
+# Exemplo de uso de Balneario a Gramado 
+distancia = 508  # em quilômetros 
+velocidade_media = 120  # em quilômetros por hora
+
+tempo_viagem = calcular_tempo_viagem(distancia, velocidade_media)
+print(f"O tempo estimado de viagem é de {tempo_viagem:.2f} horas.")

f44c0a4aca08f8912667892c17ee62e424296854
edilson gomes de lima <edskateicevida@gmail.com>
2023-06-03 16:59:34 -0300

    add multiplicação

---
+++ format01.py
@@ -0,0 +1,12 @@
+# multilicação
+
+numero = float(input("Digite um número: "))
+
+resultado = numero ** 2
+
+# Exibe o resultado
+print("O resultado é:", resultado)
+
+# divisão
+
+numero = float(input("Digite um número: "))

8e51f49a925d4aa5697876bf90fd1f1c349b42ce
edilson gomes de lima <edskateicevida@gmail.com>
2023-06-03 17:49:24 -0300

    add digite o nome

---
+++ format02.py
@@ -0,0 +1,4 @@
+aluno = input("Digite seu nome: ")
+periodo_corrente = input("Digite seu período corrente: ")
+ira = input("Digite seu IRA(Índice de Rendimento Acadêmico): ")
+laurea = input("Informe se você é o aluno laureado(a) da turma: ")
